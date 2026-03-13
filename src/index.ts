72cfc1740f607d7ae239fb677b0f6604f02530ea
payal <payalbalotra24@gmail.com>
2026-03-13 16:25:08 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,48 @@
+import { PrismaClient } from '../src/generated/prisma/client.js';
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const port = 3000;
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+app.use(limiter);
+app.use(helmet());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  }),
+);
 
-app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: 'ok',
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
-    services: {
-      server: 'up',
-      //   database: dbStatus(),
-    },
-  });
+app.use(express.json());
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+
+app.get('/health', async (req, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+
+    res.json({
+      status: 'ok',
+      database: 'connected',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 'error',
+      database: 'disconnected',
+    });
+  }
 });
 
 app.listen(port, () => {
